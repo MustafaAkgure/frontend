db8d8565942cfd014e4a64c97f22ac312c57a434
kafeinmakgure <MuMi@22422008>
2019-10-15 17:16:17 +0300

    second commit

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { User } from '../model/user';
-import { Observable } from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class UserService {
 
-  private usersUrl: string;
+  private requestURL: string = environment.baseURL + '/users';
 
-  constructor(private http: HttpClient) {
-    this.usersUrl = 'http://localhost:8080/users';
-  }
+  constructor(private http: HttpClient) { }
 
-  public findAll(): Observable<User[]> {
-    return this.http.get<User[]>(this.usersUrl);
+  public findAll() {
+    return this.http.get<User[]>(this.requestURL);
   }
 
   public save(user: User) {
-    return this.http.post<User>(this.usersUrl, user);
+    return this.http.post<User>(this.requestURL, user);
   }
 }
